58678d26642b4aaf615d212f29256c351799767b
abrshtg <abrshtgam@gmail.com>
2022-08-10 12:52:13 +0300

    update ALLOWED_HOST setting

--- django_project/settings.py
+++ django_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".herokuapp.com", "192.168.43.59"]
+ALLOWED_HOSTS = ["evening-plateau-20807.herokuapp.com/", "127.0.0.1", "localhost", ".herokuapp.com", "192.168.43.59"]
 
 
 # Application definition
