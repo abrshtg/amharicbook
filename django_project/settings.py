28aacd8e66d50fab14518712f52602a97d5bf370
abrshtg <abrshtgam@gmail.com>
2022-08-17 12:40:25 +0300

    yeah still working on it

--- django_project/settings.py
+++ django_project/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DJANGO_DEBUG", default=False)
+DEBUG = env.bool("DJANGO_DEBUG", default=True)
 
 ALLOWED_HOSTS = [
     "evening-plateau-20807.herokuapp.com/",
